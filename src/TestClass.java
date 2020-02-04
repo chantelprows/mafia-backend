4f29d8f8aece8bac6dc0a720fb44e32abd893659
Chantel Prows <chantelp44@gmail.com>
2020-02-04 11:26:42 -0700

    working test lambda

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,2 +1,12 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
 public class TestClass {
+
+    public boolean reached(Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("Entering reached");
+
+        return true;
+    }
 }
