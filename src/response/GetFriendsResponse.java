ff61cbe1bdad5de9d0686fb42525d4f6b84a17a6
roscoeevans <roscoe_evans@icloud.com>
2020-02-16 16:32:56 -0700

    added getFriends helper

--- src/response/GetFriendsResponse.java
+++ src/response/GetFriendsResponse.java
@@ -2,12 +2,15 @@ package response;
 
 public class GetFriendsResponse {
     public String[] friends;
+    public String message;
 
     public GetFriendsResponse(String[] friends) {
         this.friends = friends;
+        this.message = null;
     }
 
     public GetFriendsResponse() {
         this.friends = null;
+        this.message = null;
     }
 }
